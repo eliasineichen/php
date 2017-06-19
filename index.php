56369bffaa9a6a34ca98580dd1b934a5041b5820
Elias Ineichen <eineichen@edp.ch>
2017-06-19 17:23:30 +0200

    Variablen

--- index.php
+++ index.php
@@ -9,8 +9,40 @@
     <body>
 
         <?php
-        echo "My first PHP script!";
-        ?>
+            // GLOBALS
+            $globaleVariable1 = 1;
+            $globaleVariable2 = 2;
 
+            function calc() {
+                // Definiert Globale Variable als lokale Variable.
+                global $globaleVariable1, $globaleVariable2;
+
+                // Greift direkt auf Globale Variable zu.
+                $GLOBALS['globaleVariable3'] = $globaleVariable1 + $globaleVariable2;
+            }
+
+            function increment() {
+                static $index = 0;
+                $index++;
+                $GLOBALS['index'] = $index;
+            }
+
+            calc();
+            
+            increment();
+            increment();            
+            increment();
+
+            //Gibt Text aus
+            echo "My first PHP script!", 
+            "<br><br>", 
+            "Globale Variable: ",
+            $globaleVariable3,
+            "<br><br>",
+            "Statische Variable: ",
+            $index;
+
+
+        ?>
     </body>
 </html>
