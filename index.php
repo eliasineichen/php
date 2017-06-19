da0f52ab7a35ed18fd8d486240254fb96753505b
Elias Ineichen <eineichen@edp.ch>
2017-06-19 14:45:37 +0200

    First PHP-Page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>PHP</title>
+    
+    </head>
+    <body>
+
+        <?php
+        echo "My first PHP script!";
+        ?>
+
+    </body>
+</html>
