ccd76969b1bcb2854946bd5d1655dcb26a9d44dd
Elias Ineichen <eineichen@edp.ch>
2017-06-21 11:19:44 +0200

    Echo, Print

--- index.php
+++ index.php
@@ -14,11 +14,14 @@
             $globaleVariable2 = 2;
 
             function calc() {
+                $lokal = "Hallo, ich bin eine lokale Variable! ;)";
                 // Definiert Globale Variable als lokale Variable.
                 global $globaleVariable1, $globaleVariable2;
 
                 // Greift direkt auf Globale Variable zu.
                 $GLOBALS['globaleVariable3'] = $globaleVariable1 + $globaleVariable2;
+
+                echo $lokal;
             }
 
             function increment() {
@@ -34,15 +37,38 @@
             increment();
 
             //Gibt Text aus
-            echo "My first PHP script!", 
-            "<br><br>", 
-            "Globale Variable: ",
-            $globaleVariable3,
-            "<br><br>",
-            "Statische Variable: ",
-            $index;
+            echo
+                "<br><br>", 
+                "Globale Variable: ",
+                $globaleVariable3,
+                "<br><br>",
+                "Statische Variable: ",
+                $index;
 
+            // Echo
+            echo "<h1>ECHOS:</h1>";
+            echo "Hallo, ich bin <b>FETT</b>.";
+            echo "<br>";
+            echo "Dieser", " ", "String", " ", "wurde", " ", "zusammengesetzt!";
+            echo ("<br>");
+            echo ("Dies ist ein Echo in Klammern...");
+            echo ("<br>");
 
+            $name = "Ineichen";
+            $vorname = "Elias";
+            $eins = 1;
+            $vier = 4;
+
+            echo "Hallo, ich bin $vorname $name";
+            echo "<br>";
+            echo $eins, $vier;
+            echo "<br>";
+            echo $eins + $vier;
+            
+            // PRINT
+            print "<h1>PRINTS:</h1>";
+            print "Einfacher Print...";
+            print ("Print in Klammern");
         ?>
     </body>
 </html>
