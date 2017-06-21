9cf1eb5e02bad6879e15eac3b2bdcb69ee9f5299
Elias Ineichen <eineichen@edp.ch>
2017-06-21 12:16:15 +0200

    Datentypen, String-Funktionen, Constants

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="de">
     <head>
 
+        <meta charset="utf-8">
         <title>PHP</title>
     
     </head>
     <body>
 
         <?php
+            // Variablen
+            echo "<h1>Variablen:</h1>";
+
             // GLOBALS
             $globaleVariable1 = 1;
             $globaleVariable2 = 2;
@@ -68,7 +72,82 @@
             // PRINT
             print "<h1>PRINTS:</h1>";
             print "Einfacher Print...";
+            print "<br>";
             print ("Print in Klammern");
+
+            // Datentypen
+            print ("<h1>Datentypen</h1>");
+
+            $string = "Ich bin ein String";
+            $int = -5275;
+            $float = 2345.34534;
+            $boolean = true;
+            $array = array("Audi", "BMW", "Mercedes", "Ford", "Chevrolet");
+            $null = NULL;
+
+            // Klasee
+            class Car {
+
+                // Konstruktor
+                function Car() {
+                    $this->Marke = "VW";
+                    $this->Model = "Golf";
+                }
+            }
+
+            // Create Object
+            $object = new Car();
+
+            echo "String: ", var_dump($string), "<br>";
+            echo "Int: ", var_dump($int), "<br>";
+            echo "Float: ", var_dump($float), "<br>";
+            echo "Boolean: ", var_dump($boolean), "<br>";
+            echo "Array: ", var_dump($array), "<br>";
+            echo "Object: ", var_dump($object), "<br>";
+            echo "Null: ", var_dump($null), "<br>";
+
+            // String-Functions
+            echo "<h1>String-Funktioinen:</h1>";
+            $simpleString = "Hello world!";
+
+            // Länge des Strings
+            echo "Länge des Strings: ";
+            echo strlen($simpleString);
+            echo "<br>";
+
+            // Anzahl Wörter
+            echo "Anzahl Wörter: ";
+            echo str_word_count($simpleString);
+            echo "<br>";
+
+            // String umkehren
+            echo "String umgekehrt: ";
+            echo strrev($simpleString);
+            echo "<br>";
+
+            // String Position
+            echo "Position von 'world': ";
+            echo strpos($simpleString, "world");
+            echo "<br>";
+
+            // String ersetzen
+            echo "Ersetst das Wort 'world': ";
+            echo str_replace("world", "Dolly", $simpleString);
+            echo "<br>";
+
+            // Constants
+            echo "<h1>Constants:</h1>";
+            define("konstante", "Hallo Ich bin eine Konstante!");
+            echo konstante;
+            echo "<br><br>";
+
+            function irgendetwas () {
+                echo "Konstanten sind global. Jetzt wird sie innerhalb einer Funktino ausgegeben: ";
+                echo "<br>";
+                echo konstante;
+            }
+
+            irgendetwas();
         ?>
     </body>
 </html>
